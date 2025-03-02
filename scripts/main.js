a00bd82fa0b7a49d3bcf57410e3ba208abd2f02f
Alauddin Munshi <amunshi.dev@gmail.com>
2025-03-02 20:35:21 +0400

    Add devBoard complete functionality except randomColor

--- scripts/main.js
+++ scripts/main.js
@@ -1,80 +1,96 @@
-let completeBtn = document.querySelectorAll('.completeBtn')
-
 //updating day date
 function updateDate() {
- const now = new Date();
- 
- const dayOptions = { weekday: "short" };
- const dateOptions = { month: "short", day: "numeric", year: "numeric" };
- 
- document.getElementById("day").textContent = now.toLocaleDateString("en-US", dayOptions) + " ,";
- document.getElementById("date").textContent = now.toLocaleDateString("en-US", dateOptions);
+  const now = new Date();
+
+  const dayOptions = { weekday: "short" };
+  const dateOptions = { month: "short", day: "numeric", year: "numeric" };
+
+  document.getElementById("day").textContent =
+    now.toLocaleDateString("en-US", dayOptions) + " ,";
+  document.getElementById("date").textContent = now.toLocaleDateString(
+    "en-US",
+    dateOptions
+  );
 }
 updateDate();
 
 // discover to blogs
-const discoverBtn = document.getElementById('discoverBtn')
-console.log(discoverBtn)
-discoverBtn.addEventListener('click', () => {
- window.location.href = './QnA.html'
-})
+const discoverBtn = document.getElementById("discoverBtn");
+discoverBtn.addEventListener("click", () => {
+  window.location.href = "./blogs.html";
+});
 
-//remaining task
-const taskRemain = document.getElementById('remaining-task')
+let remainingTask = document.querySelectorAll(".task-card").length;
+let completedTask = 0;
 
-//completed task
-let taskCompleted = document.getElementById('completed-task')
+// let remainStatus = document.getElementById("remaining-task");
+let completeStatus = document.getElementById("completed-task").textContent;
 
-console.log(taskRemain, taskCompleted)
+const completeBtn = document.querySelectorAll(".completeBtn");
+const logContainer = document.getElementById("logContainer");
 
-function increaseTask() {
- let converted = Number(taskCompleted.textContent)
- converted++
- return taskCompleted.textContent = String(converted)
+function updateTaskCounts() {
+  alert("Board had been updated successfully.");
+
+  // disabling the button
+  this.disabled = true;
+  this.classList.add("opacity-20");
+
+  //targetting card
+  const taskCard = this.closest(".task-card");
+  //  targetting the task-name
+  const taskName = taskCard.children[1].textContent;
+
+  //remaining task
+  const taskRemain = document.getElementById("remaining-task");
+  //completed task
+  const taskComplete = document.getElementById("completed-task");
+
+  remainingTask--;
+  taskRemain.textContent = remainingTask;
+
+  completedTask++;
+  taskComplete.textContent = completedTask + parseInt(completeStatus);
+
+  // activity log
+  // add log entry
+  function addLogEntry() {
+    const newLogEntry = document.createElement("p");
+    const currentTime = formatTime(new Date());
+
+    newLogEntry.classList.add(
+      "text-text/70",
+      "p-4",
+      "bg-background",
+      "rounded-lg"
+    );
+    newLogEntry.textContent = `You have Completed The Task ${taskName} at ${currentTime}`;
+    logContainer.appendChild(newLogEntry);
+  }
+  addLogEntry();
 }
-
-for (let e = 0; e < completeBtn.length; e++) {
- completeBtn[e].addEventListener('click', decreaseTask)
- 
-}
-function decreaseTask() {
- let converted = Number(taskRemain.textContent)
- converted--
- taskRemain.textContent = String(converted).padStart(2, '0')
-}
-
-//activity log
-const clearButton = document.getElementById('clearBtn');
-const logContainer = document.getElementById('logContainer');
 
 function formatTime(date) {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  
+  const ampm = hours >= 12 ? "PM" : "AM";
+
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
   return `${hours}:${minutes}:${seconds} ${ampm}`;
 }
 
-// add log entry 
-function addLogEntry() {
-  const newLogEntry = document.createElement('p');
-  const currentTime = formatTime(new Date());
-  
-  newLogEntry.classList.add('text-text/70', 'p-4', 'bg-background', 'rounded-lg');
-  newLogEntry.textContent = `You have Completed The Task Add Dark Mode at ${currentTime}`;
-  logContainer.appendChild(newLogEntry);
-}
+completeBtn.forEach((button) => {
+  button.addEventListener("click", updateTaskCounts);
+});
 
+const clearButton = document.getElementById("clearBtn");
 // clear  entries
 function clearLogs() {
-  logContainer.innerHTML = '';
+  logContainer.innerHTML = "";
 }
-
-// addButton.addEventListener('click', addLogEntry);
-clearButton.addEventListener('click', clearLogs);
+clearButton.addEventListener("click", clearLogs);
